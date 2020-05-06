09b6d1fc52fcf0d70e5f418ea05730a7e07dccd8
Yamel Senih <ysenih@erpya.com>
2020-05-06 15:06:31 -0400

    Rename main class

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License                 *
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.            *
  ************************************************************************************/
-class BusinessData {
+class SystemCore {
 
   /**
    * Constructor, No authentication required
@@ -163,4 +163,4 @@ class BusinessData {
   }
 }
 
-module.exports = BusinessData;
+module.exports = SystemCore;
--- src/convertUtil.js
+++ src/convertUtil.js
@@ -499,29 +499,6 @@ const convertUtil = {
       };
     },
 
-    convertCurrencyFromGRPC(currencyToConvert) {
-      if (currencyToConvert) {
-        return {
-          currencyId: currencyToConvert.getId(),
-          currencyUuid: currencyToConvert.getUuid(),
-          iSOCode: currencyToConvert.getIsocode(),
-          curSymbol: currencyToConvert.getCursymbol(),
-          description: currencyToConvert.getDescription(),
-          stdPrecision: currencyToConvert.getStdprecision(),
-          costingPrecision: currencyToConvert.getCostingprecision()
-        };
-      }
-      return {
-        currencyId: undefined,
-        currencyUuid: undefined,
-        iSOCode: undefined,
-        curSymbol: undefined,
-        description: undefined,
-        stdPrecision: undefined,
-        costingPrecision: undefined
-      };
-    },
-
     convertCountryFromGRPC(countryToConvert) {
       if (countryToConvert) {
         return {
@@ -1274,7 +1251,86 @@ const convertUtil = {
       };
     },
 
-    convertDocumentAction(documentActionToConvert) {
+    convertSalesRepresentativeFromGRPC(salesRepresentativeToConvert) {
+      if (salesRepresentativeToConvert) {
+        return {
+          uuid: salesRepresentativeToConvert.getUuid(),
+          id: salesRepresentativeToConvert.getId(),
+          name: salesRepresentativeToConvert.getName(),
+          description: salesRepresentativeToConvert.getDescription()
+        }
+      }
+      return {
+        uuid: undefined,
+        id: undefined,
+        name: undefined,
+        description: undefined
+      }
+    },
+
+    // Business Partner
+    convertBusinessPartnerFromGRPC(businessPartnerToConvert) {
+      if (businessPartnerToConvert) {
+        return {
+          uuid: businessPartnerToConvert.getUuid(),
+          id: businessPartnerToConvert.getId(),
+          value: businessPartnerToConvert.getValue(),
+          taxId: businessPartnerToConvert.getTaxid(),
+          duns: businessPartnerToConvert.getDuns(),
+          naics: businessPartnerToConvert.getNaics(),
+          name: businessPartnerToConvert.getName(),
+          lastName: businessPartnerToConvert.getLastname(),
+          description: businessPartnerToConvert.getDescription()
+        };
+      }
+      return {
+        uuid: undefined,
+        id: undefined,
+        value: undefined,
+        taxId: undefined,
+        duns: undefined,
+        naics: undefined,
+        name: undefined,
+        lastName: undefined,
+        description: undefined
+      };
+    },
+
+    convertDocumentTypeFromGRPC(documentTypeToConvert) {
+      if (documentTypeToConvert) {
+        return {
+          uuid: documentTypeToConvert.getUuid(),
+          id: documentTypeToConvert.getId(),
+          name: documentTypeToConvert.getName(),
+          printerName: documentTypeToConvert.getPrintername(),
+          description: documentTypeToConvert.getDescription()
+        };
+      }
+      return {
+        uuid: undefined,
+        id: undefined,
+        name: undefined,
+        printerName: undefined,
+        description: undefined
+      };
+    },
+
+    convertDocumentStatusFromGRPC(documentStatusToConvert) {
+      if (documentStatusToConvert) {
+        return {
+          value: documentStatusToConvert.getValue(),
+          name: documentStatusToConvert.getName(),
+          description: documentStatusToConvert.getDescription()
+        };
+      }
+      return {
+        value: undefined,
+        name: undefined,
+        description: undefined
+      };
+    },
+
+    convertDocumentActionFromGRPC(documentActionToConvert) {
       if (documentActionToConvert) {
         return {
           value: documentActionToConvert.getValue(),
@@ -1289,20 +1345,154 @@ const convertUtil = {
       };
     },
 
-    convertDocumentStatus(documentStatusToConvert) {
-      if (documentStatusToConvert) {
+    convertProductPriceFromGRPC(productPriceToConvert) {
+      if (productPriceToConvert) {
         return {
-          value: documentStatusToConvert.getValue(),
-          name: documentStatusToConvert.getName(),
-          description: documentStatusToConvert.getDescription()
+          currency: convertUtils.convertCurrencyFromGRPC(productPriceToConvert.getCurrency()),
+          taxRate: convertUtils.convertTaxRateFromGRPC(productPriceToConvert.getTaxrate()),
+          product: convertUtils.convertProductFromGRPC(productPriceToConvert.getProduct()),
+          priceList: convertUtils.convertDecimalValue(productPriceToConvert.getPricelist()),
+          priceStd: convertUtils.convertDecimalValue(productPriceToConvert.getPricestd()),
+          priceLimit: convertUtils.convertDecimalValue(productPriceToConvert.getPricelimit()),
+          priceListName: productPriceToConvert.getPricelistname(),
+          isTaxIncluded: productPriceToConvert.getIstaxincluded(),
+          validFrom: productPriceToConvert.getValidfrom(),
+          pricePrecision: productPriceToConvert.getPriceprecision(),
+          quantityOnHand: convertUtils.convertDecimalValue(productPriceToConvert.getQuantityonhand()),
+          quantityReserved: convertUtils.convertDecimalValue(productPriceToConvert.getQuantityreserved()),
+          quantityOrdered: convertUtils.convertDecimalValue(productPriceToConvert.getQuantityordered()),
+          quantityAvailable: convertUtils.convertDecimalValue(productPriceToConvert.getQuantityavailable())
         };
       }
       return {
+        product: undefined,
+        currency: undefined,
+        taxRate: undefined,
+        priceList: undefined,
+        priceStd: undefined,
+        priceLimit: undefined,
+        priceListName: undefined,
+        isTaxIncluded: undefined,
+        validFrom: undefined,
+        pricePrecision: undefined,
+        quantityOnHand: undefined,
+        quantityReserved: undefined,
+        quantityOrdered: undefined,
+        quantityAvailable: undefined
+      };
+    },
+
+    convertCurrencyFromGRPC(currencyToConvert) {
+      if(currencyToConvert) {
+        return {
+          currencyId: currencyToConvert.getId(),
+          currencyUuid: currencyToConvert.getUuid(),
+          iSOCode: currencyToConvert.getIsocode(),
+          curSymbol: currencyToConvert.getCursymbol(),
+          description: currencyToConvert.getDescription(),
+          stdPrecision: currencyToConvert.getStdprecision(),
+          costingPrecision: currencyToConvert.getCostingprecision()
+        };
+      }
+      return {
+        currencyId: undefined,
+        currencyUuid: undefined,
+        iSOCode: undefined,
+        curSymbol: undefined,
+        description: undefined,
+        stdPrecision: undefined,
+        costingPrecision: undefined
+      };
+    },
+
+    convertTaxRateFromGRPC(taxRateToConvert) {
+      //  Tax rate
+      if (taxRateToConvert) {
+        return {
+          name: taxRateToConvert.getName(),
+          description: taxRateToConvert.getDescription(),
+          taxIndicator: taxRateToConvert.getTaxindicator(),
+          rate: convertUtils.convertDecimalValue(taxRateToConvert.getRate())
+        };
+      }
+      return {
+        name: undefined,
+        description: undefined,
+        taxIndicator: undefined,
+        rate: undefined
+      };
+    },
+
+    convertProductFromGRPC(productToConvert) {
+      if (productToConvert) {
+        return {
+          uuid: productToConvert.getUuid(),
+          id: productToConvert.getId(),
+          value: productToConvert.getValue(),
+          name: productToConvert.getName(),
+          help: productToConvert.getHelp(),
+          documentNote: productToConvert.getDocumentnote(),
+          uomName: productToConvert.getUomname(),
+          productType: productToConvert.getProducttype(),
+          isStocked: productToConvert.getIsstocked(),
+          isDropShip: productToConvert.getIsdropship(),
+          isPurchased: productToConvert.getIspurchased(),
+          isSold: productToConvert.getIssold(),
+          imageURL: productToConvert.getImageurl(),
+          productCategoryName: productToConvert.getProductcategoryname(),
+          productGroupName: productToConvert.getProductgroupname(),
+          productClassName: productToConvert.getProductclassname(),
+          productClassificationName: productToConvert.getProductclassificationname(),
+          weight: convertUtils.convertDecimalValue(productToConvert.getWeight()),
+          volume: convertUtils.convertDecimalValue(productToConvert.getVolume()),
+          upc: productToConvert.getUpc(),
+          sku: productToConvert.getSku(),
+          shelfWidth: productToConvert.getShelfwidth(),
+          shelfHeight: convertUtils.convertDecimalValue(productToConvert.getShelfheight()),
+          shelfDepth: productToConvert.getShelfdepth(),
+          unitsPerPack: productToConvert.getUnitsperpack(),
+          unitsPerPallet: convertUtils.convertDecimalValue(productToConvert.getUnitsperpallet()),
+          guaranteeDays: productToConvert.getGuaranteedays(),
+          descriptionURL: productToConvert.getDescriptionurl(),
+          versionNo: productToConvert.getVersionno(),
+          taxCategory: productToConvert.getTaxcategory(),
+          description: productToConvert.getDescription()
+        };
+      }
+      return {
+        uuid: undefined,
+        id: undefined,
         value: undefined,
         name: undefined,
+        help: undefined,
+        documentNote: undefined,
+        uomName: undefined,
+        productType: undefined,
+        isStocked: undefined,
+        isDropShip: undefined,
+        isPurchased: undefined,
+        isSold: undefined,
+        imageURL: undefined,
+        productCategoryName: undefined,
+        productGroupName: undefined,
+        productClassName: undefined,
+        productClassificationName: undefined,
+        weight: undefined,
+        volume: undefined,
+        upc: undefined,
+        sku: undefined,
+        shelfWidth: undefined,
+        shelfHeight: undefined,
+        shelfDepth: undefined,
+        unitsPerPack: undefined,
+        unitsPerPallet: undefined,
+        guaranteeDays: undefined,
+        descriptionURL: undefined,
+        versionNo: undefined,
+        taxCategory: undefined,
         description: undefined
       };
-    }
+    },
   }
 
   module.exports = convertUtils
